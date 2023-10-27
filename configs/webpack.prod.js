b93bf18b6f84e8e9944de175a20eda523da0b862
ddchef <dev@xjie.me>
2023-10-27 14:07:52 +0800

    build: 添加公共代码提取方式

--- configs/webpack.prod.js
+++ configs/webpack.prod.js
@@ -18,7 +18,28 @@ module.exports = merge(common, {
         extractComments: false,
         minify: TerserPlugin.swcMinify
       })
-    ]
+    ],
+    splitChunks: {
+      chunks: 'all',
+      // 缓存分组
+      cacheGroups: {
+        verdors: {
+          name: 'verdor',
+          test: /node_modules/,  // 设置命中目录规则
+          priority: 1, // 优先级，数值越大，优先级越高
+          minSize: 10, // 小于这个大小的文件，不分割
+          minChunks: 1 // 最少复用几次，这里意思是只要用过一次就分割出来
+        },
+        // 公共模块
+        common: {
+          name: 'common',
+          minChunks: 2,
+          priority: 0,
+          minSize: 10,
+          minChunks: 2  // 只要引用过2次，就分割成公共代码
+        }
+      }
+    }
   },
   plugins: [
     new CopyPlugin({
