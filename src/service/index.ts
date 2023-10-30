96318dace8f48fb13d816098b9029be164d75c7b
ddchef <dev@xjie.me>
2023-10-30 14:07:09 +0800

    build: 修改 baseurl

--- src/service/index.ts
+++ src/service/index.ts
@@ -2,7 +2,7 @@ import Request from '@/libs/fetch';
 
 function createService() {
   const defaultConfig = {
-    baseURL: process.env.API_BASE_PATH || '',
+    baseURL: process.env.API_BASE_PATH || '/',
     timeout: Number(process.env.TIME_OUT || 1000 * 60)
   };
   const request = new Request(defaultConfig);
